e0fa4071df62b91fae55654afac250c542e727a1
Diegoodlvv <deoliveiradiego953@gmail.com>
2025-11-07 16:41:47 +0100

    Création du Model Utilisateur

--- src/Models/Utilisateur.php
+++ src/Models/Utilisateur.php
@@ -1,16 +1,69 @@
-<?php 
+<?php
 
 namespace App\Models;
 
-class Utilisateur 
+use App\Enum\EnumRoleUtilisateur;
+
+class Utilisateur
 {
 
-    public function __construct
-    (
+    public function __construct(
         protected string $nom,
         protected string $email,
         protected string $mdp,
-        protected 
-    )
-    {}
+        protected EnumRoleUtilisateur $role,
+        protected ?int $id = null
+    ) {}
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nouveauNom): void
+    {
+        $this->nom = $nouveauNom;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getMdp(): string
+    {
+        return $this->mdp;
+    }
+
+    public function setMdp(string $nouveauMdp): void
+    {
+        $this->mdp = password_hash($nouveauMdp, PASSWORD_BCRYPT);
+    }
+
+    public function getRole(): EnumRoleUtilisateur
+    {
+        return $this->role;
+    }
+
+    public function setRole(EnumRoleUtilisateur $nouveauRole): void
+    {
+        $this->role = $nouveauRole;
+    }
+
+    public static function arrayToUser(array $dataUser): Utilisateur
+    {
+        $role = EnumRoleUtilisateur::from($dataUser['role']);
+        return new Utilisateur(
+            $dataUser['nom'],
+            $dataUser['email'],
+            $dataUser['mot_de_passe'],
+            $role,
+            $dataUser['id']
+        );
+    }
 }
